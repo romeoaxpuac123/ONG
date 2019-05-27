2d2cd7faa60a82d207ccc3262c3276eeaa01193d
Romeo <misinicialesbray@gmail.com>
2019-05-26 18:59:59 -0600

    REGISTRO CONOCIDO

---
+++ GestionPreventivos/RegistroBeneficiario.aspx
@@ -0,0 +1,83 @@
+﻿ <%@ Page Language="C#" AutoEventWireup="true" CodeBehind="RegistroBeneficiario.aspx.cs" Inherits="GestionPreventivos.RegistroBeneficiario" %>
+
+<link href="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+<script src="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
+<script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+<!------ Include the above in your HEAD tag ---------->
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>REGISTRO DELITO</title>
+   <!--Made with love by Mutiullah Samim -->
+   
+	<!--Bootsrap 4 CDN-->
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+    
+    <!--Fontawesome CDN-->
+	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css" integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU" crossorigin="anonymous">
+
+	<!--Custom styles-->
+	<link rel="stylesheet" type="text/css" href="styles.css">
+</head>
+<body>
+<div class="container">
+	<div class="d-flex justify-content-center h-100">
+		<div class="card">
+			<div class="card-header">
+				<h3>REGISTRO BENEFICIARIO</h3>
+                <!--
+				<div class="d-flex justify-content-end social_icon">
+					<span><i class="fab fa-facebook-square"></i></span>
+					<span><i class="fab fa-google-plus-square"></i></span>
+					<span><i class="fab fa-twitter-square"></i></span>
+				</div>
+                    -->
+			</div>
+		<div class="card-body">
+				<form id="form1" runat="server">
+					<div class="input-group form-group">
+						<div class="input-group-prepend">
+							<span class="input-group-text"><i class="fas fa-user"></i></span>
+						</div>
+                        <asp:TextBox type="text" class="form-control" placeholder="PRIVADO" autocomplete="off" ID="TextBox1" runat="server" Style="text-transform: uppercase"></asp:TextBox>
+						
+					</div>
+
+
+					<div class="input-group form-group">
+						<div class="input-group-prepend">
+							<span class="input-group-text"><i class="fas fa-user"></i></span>
+						</div>
+						
+					     <asp:TextBox type="text" class="form-control" placeholder="NOMBRE BENEFICIADO" autocomplete="off" ID="TextBox2" runat="server" Style="text-transform: uppercase"></asp:TextBox>
+                    </div>
+
+                    <div class="input-group form-group">
+						<div class="input-group-prepend">
+							<span class="input-group-text"><i class="fas fa-edit"></i></span>
+						</div>
+						
+					     <asp:TextBox type="text" class="form-control" placeholder="DIRECCION" autocomplete="off" ID="TextBox3" runat="server" Style="text-transform: uppercase"></asp:TextBox>
+                    </div>
+                   
+                    <div class="input-group form-group">
+						<div class="input-group-prepend">
+							<span class="input-group-text"><i class="fas fa-edit"></i></span>
+						</div>
+						
+					     <asp:TextBox type="text" class="form-control" placeholder="TELEFONO" autocomplete="off" ID="TextBox4" runat="server" Style="text-transform: uppercase"></asp:TextBox>
+                    </div>        
+                   
+                         <div class="form-group">
+						  <asp:Button type="submit" value="REGISTRO" class="btn float-right login_btn" ID="Button1" runat="server" Text="REGISTRO" OnClick="Button1_Click"  />
+					</div>
+				</form>
+			</div>
+			
+		</div>
+	    <br />
+	</div>
+</div>
+</body>
+</html>
---
+++ GestionPreventivos/RegistroBeneficiario.aspx.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace GestionPreventivos
+{
+    public partial class RegistroBeneficiario : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            if (EXistePrivado(TextBox1.Text) == true)
+            {
+                InsetarBeneficiario(CodigoPrivado(TextBox1.Text), TextBox2.Text, TextBox3.Text, TextBox4.Text);
+                MessageBox.Show("INFORMACIÓN REGISTRADA");
+            }
+            else
+            {
+                MessageBox.Show("NO EXISTE EL PRIVADO EN LA BD");
+            }
+        }
+        public int CodigoPrivado(String Nombre)
+        {
+            SqlConnection conxx = new SqlConnection("Data Source=.;Initial Catalog = BDONG;Trusted_Connection=true;");
+            conxx.Open();
+            SqlCommand comxx = new SqlCommand(); // Create a object of SqlCommand class
+            comxx.Connection = conxx; //Pass the connection object to Command
+            comxx.CommandType = CommandType.StoredProcedure; // We will use stored procedure.
+            comxx.CommandText = "EXISTEPRIVADO2"; //Stored Procedure Name
+            comxx.Parameters.Add("@NOMBRE", SqlDbType.NVarChar).Value = Nombre;
+            comxx.ExecuteNonQuery();
+            SqlDataReader readerxx = comxx.ExecuteReader();
+            if (readerxx.HasRows)
+            {
+                while (readerxx.Read())
+                {
+                    return readerxx.GetInt32(0);
+
+                }
+            }
+            conxx.Close();
+            return 1;
+        }
+        public Boolean EXistePrivado(String Nombre)
+        {
+            SqlConnection conxx = new SqlConnection("Data Source=.;Initial Catalog = BDONG;Trusted_Connection=true;");
+            conxx.Open();
+            SqlCommand comxx = new SqlCommand(); // Create a object of SqlCommand class
+            comxx.Connection = conxx; //Pass the connection object to Command
+            comxx.CommandType = CommandType.StoredProcedure; // We will use stored procedure.
+            comxx.CommandText = "EXISTEPRIVADO2"; //Stored Procedure Name
+            comxx.Parameters.Add("@NOMBRE", SqlDbType.NVarChar).Value = Nombre;
+            comxx.ExecuteNonQuery();
+            SqlDataReader readerxx = comxx.ExecuteReader();
+            int codigo = 0;
+            if (readerxx.HasRows)
+            {
+                while (readerxx.Read())
+                {
+                    codigo = readerxx.GetInt32(0);
+
+                }
+            }
+            conxx.Close();
+            if (codigo > 0)
+                return true;
+            return false;
+        }
+        public void InsetarBeneficiario(int Privado, String Beneficiario,
+            string telefono, string direccion)
+        {
+            SqlConnection conxx = new SqlConnection("Data Source=.;Initial Catalog = BDONG;Trusted_Connection=true;");
+            conxx.Open();
+            SqlCommand comxx = new SqlCommand(); // Create a object of SqlCommand class
+            comxx.Connection = conxx; //Pass the connection object to Command
+            comxx.CommandType = CommandType.StoredProcedure; // We will use stored procedure.
+            comxx.CommandText = "INSERTARBENEFICIARIO"; //Stored Procedure Name
+            comxx.Parameters.Add("@CODIGO_PRIVADO", SqlDbType.NVarChar).Value = Privado;
+            comxx.Parameters.Add("@NOMBRE_BENEFICIARIO", SqlDbType.NVarChar).Value = Beneficiario;
+            comxx.Parameters.Add("@DIRECCION ", SqlDbType.NVarChar).Value = telefono;
+            comxx.Parameters.Add("@TELEFONO ", SqlDbType.NVarChar).Value = direccion;
+        comxx.ExecuteNonQuery();
+            conxx.Close();
+        }
+
+    }
+}
